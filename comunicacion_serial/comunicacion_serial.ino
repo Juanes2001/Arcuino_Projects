0aa7c2e1ba503e6cc2f2cbfab65dc546e725ae23
Juanes2001 <111066079+Juanes2001@users.noreply.github.com>
2024-12-16 19:01:12 -0500

    Actualizacion

--- comunicacion_serial/comunicacion_serial.ino
+++ comunicacion_serial/comunicacion_serial.ino
@@ -13,8 +13,10 @@ const char* help_menu = "\n=================================COMANDOS============
                         "\n///INSERTAR EL COMANDO CON LA SIGUIENTE ESTRUCTURA QUE SE ENCUENTRA ABAJO///\n"
                         "\n///               Y TERMINARLA CON EL SIGUIENTE CARACTER @               ///\n"
                         "\n///             1)  step_motor #steps #dir --> 1 CW, 0 CCW               ///\n" // Anti horario es con pasos negativos
-                        "\n///             2)  start_measurements                                   ///\n";
+                        "\n///             2)  start_measurements                                   ///\n"
+                        "\n///             3)  on_lux                                               ///\n";
 
+int cuentas = 3;
 
 // Mensajes a mandar
 int IN1 = 19;
@@ -27,24 +29,31 @@ const int stepsPerRevolution = 2048;
 
 BH1750 lightMeter;
 float lux;
+float lux_medida;
 
+
+int counter = 0;
 int16_t steps = 0;
+int pasos_a_mover = 10;
+int pasos_max = 1500;
 char c;
 
 // Variables para comandos 
 String bufferreception;
-bool flag_start_measure = 0;
 
 int pasos_a_dar = 0;
 bool direccion = RESET;
 
 //banderas varias
 bool flag_step_motor = RESET;
+bool flag_start_measure = RESET;
+bool flag_on_lux = RESET;
 bool received_by_python = RESET;
 
 void readDatatoSend(void);
 void do_measurements (void);
-
+void processCommand(const std::string& command);
+void show_lux(bool flag);
 
  // Creamos la clase stepper y un objeto llamado motor
 Stepper motor(stepsPerRevolution, IN1,IN3,IN2,IN4);
@@ -54,7 +63,7 @@ void setup() {
   // initialize the serial port
   Serial.begin(115200);
   // set the speed at 5 rpm
-  motor.setSpeed(3);
+  motor.setSpeed(5);
 
     
     // Initiacializa el bus I2C (la librer ́ıa BH1750 no lo hace automáticamente)
@@ -77,7 +86,7 @@ void setup() {
 void loop() {
 
   // En esta parte leeremos la comunicacion serial que se envia desde python
-
+  lux_medida = lightMeter.readLightLevel();
   readDatatoSend();
 
 
@@ -89,7 +98,20 @@ void loop() {
     std::cout << "\nFin del comando... start_measurements\n" << "--||--\n";
     received_by_python = RESET;
 
-  }else if (flag_step_motor){
+  }else if (flag_on_lux){
+
+    while (counter < cuentas ){
+      show_lux(flag_on_lux);
+      received_by_python = RESET;
+      while (!received_by_python){readDatatoSend();delay(500);} 
+      
+    }
+    std::cout << "\nFin del comando... on_lux\n" << help_menu << "\nMANDADO" << "--||--\n";
+    received_by_python = RESET;
+    flag_on_lux = RESET;
+    counter = 0;
+  }
+  else if (flag_step_motor){
     if (direccion == 1){
       motor.step(pasos_a_dar);
     }else{
@@ -147,15 +169,25 @@ void processCommand(const std::string& command) {
       while (!received_by_python){readDatatoSend();delay(500);}
 
 
+    }else if (task == "on_lux") {
+      // Si estamos aqui se busca que el luxometro me regale cierta cantidad de datos cada cierto tiempo
+      std::cout << "\nComando procesado..."<< task << "\n" ;
+      std::cout << "|||||MOSTRANDO LUX, "<< cuentas << " MUESTRAS|||||"<< "--||--\n"  << std::endl;
+      flag_on_lux = SET;
+      bufferreception = "";
+      while (!received_by_python){readDatatoSend();delay(500);}
+
     }else if (task == "INICIO") {
       // Este es el comando de inicializacion, con esto podemos saber que la comunicacion serial se realizo correctamente y que el
       // Python y el ESP32 estan conectados correctamente. Se enviara el menu de opciones.
       std::cout << help_menu << std::endl; 
       bufferreception = "";
-    }else if (task == "RECIBIDO_step_motor" || task == "RECIBIDO_start_measurements"){
+    }else if (task == "RECIBIDO_step_motor" || 
+              task == "RECIBIDO_start_measurements" || 
+              task == "RECIBIDO_on_lux"){
         if (task == "RECIBIDO_step_motor") {received_by_python = SET;}
-        
         else if (task == "RECIBIDO_start_measurements"){received_by_python = SET;}
+        else if (task == "RECIBIDO_on_lux"){received_by_python = SET;}
 
         bufferreception = "";
     }else {
@@ -188,16 +220,26 @@ void readDatatoSend(void){
 void do_measurements (void){
 
   // En esta funcion se realizará las mediciones del fotodetector mientras corremos el motor cada 10 pasos
-  motor.step(-10);
+  steps = steps + pasos_a_mover;
+  motor.step((-1) * pasos_a_mover);
   lux = lightMeter.readLightLevel();
   Serial.print(lux);
   Serial.print(" ");
   Serial.println(steps);
-  steps = steps + 10;
-  if (steps == 990) {
+  if (steps == pasos_max) {
     std::cout << "\nTERMINADO\n"  << std::endl;
-    motor.step(990);
+    motor.step(pasos_max);
     steps = 0;
     flag_start_measure = RESET;
+  }
+}
+
+
+void show_lux(bool flag){
+  //   Con esta funcion solo mostraremos el estado de la lux real
+  if (flag){
+    lux_medida = lightMeter.readLightLevel();
+    std::cout << lux_medida << " lux\n" << "MANDADO" << std::endl;
+    counter++;
   }
 }
